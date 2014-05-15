d82da3b575ba43053e720a245b6a55a4e4cc36b2
Shawn S <shawn@codemastershawn.com>
2014-05-15 02:58:42 -0700

    Test project for portable utilities
    Added unit test project for validating the new PCL version of the
    library. At the moment, it only contains a test fixture for portable
    code providing functionality missing from standard PCL frameworks.

---
+++ Nutritionix.Portable.Tests/HttpUtilityFixture.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using NUnit.Framework;
+
+namespace Nutritionix.Portable.Tests
+{
+	[TestFixture]
+	class HttpUtilityFixture
+	{
+		[Test]
+		public void UrlEncode_ContainsSpaces()
+		{
+			string url = "http://example.com/path with spaces/";
+			string actual = Portable.Web.HttpUtility.UrlEncode(url);
+			string expected = System.Web.HttpUtility.UrlEncode(url);
+
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void UrlEncode_ContainsLessThanGreaterThan()
+		{
+			
+		}
+	}
+}
