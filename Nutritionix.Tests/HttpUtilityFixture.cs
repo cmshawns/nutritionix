3d3b20bfd18f4d1c7a17a42b01b2537cd16af3e0
Shawn S <shawn@codemastershawn.com>
2014-06-26 01:03:05 -0700

    Removed portable tests proj in favor of existing
    + Removed Nutritionix.Portable.Tests from solution and moved tests
        to existing project.
    + Modified test project reference to use Nutritionix.Portable,
        including making internal classes visible from the latter.

---
+++ Nutritionix.Tests/HttpUtilityFixture.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using Portable.System.Web;
+
+namespace Nutritionix.Portable.Tests
+{
+	[TestFixture]
+	class HttpUtilityFixture
+	{
+		[Test]
+		public void UrlEncode_ContainsSpaces()
+		{
+			string url = "http://example.com/path with spaces/";
+			string actual = HttpUtility.UrlEncode(url);
+			string expected = System.Web.HttpUtility.UrlEncode(url);
+
+			// TODO: Our code is encoding spaces as entities instead of '+'
+			Assert.AreEqual(expected, actual);
+		}
+
+		[Test]
+		public void UrlEncode_ContainsLessThanGreaterThan()
+		{
+			Assert.Fail("Test not written");
+		}
+	}
+}
